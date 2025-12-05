1076b8d758b250410f9f0d59031d64352dca4cdc
root <root@DESKTOP-FN0AHLA.>
2025-12-05 14:23:35 -0500

    player struct and player init

--- main.c
+++ main.c
@@ -3,6 +3,38 @@
 #include <stdio.h>
 #include "util/L_term_gfx.h"
 
+#define LEVEL_HEIGHT 64
+#define LEVEL_WIDTH 64
+
+int test_arr[LEVEL_WIDTH][LEVEL_HEIGHT]; //the (test) world
+struct player{
+    int x;
+    int y;
+    char * inv; // for later
+    int state; //0 = north, 1 = east, 2 = south, 3 = west
+
+};
+
+struct player * init_player(unsigned int x, unsigned int y, int * world[64][64]){
+    struct player * p = (struct player * )malloc(sizeof(struct player));
+    if(x<64 && y<64){ 
+        *world[x][y] = p->state;
+        p->x = x;
+        p->y = y;
+    }else{
+        *world[LEVEL_WIDTH/2][LEVEL_HEIGHT/2] = p->state;
+        p->x = LEVEL_WIDTH/2;
+        p->y = LEVEL_HEIGHT/2;
+    }
+    return p;
+}
+
+void kill_player(){
+
+}
+
+
+
 int main(){
     struct frame *test_f = frame_create(0, 32, 16);
     set_pixel(test_f, '#', 0, 0);
